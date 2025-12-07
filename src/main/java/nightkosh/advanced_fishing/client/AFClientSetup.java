5f88e73eb017a67edb633c2ecb59571508cf54ea
NightKosh <nightkosh@gmail.com>
2025-12-07 12:04:37 +0100

    refactoring

--- src/main/java/nightkosh/advanced_fishing/client/AFClientSetup.java
+++ src/main/java/nightkosh/advanced_fishing/client/AFClientSetup.java
@@ -1,13 +1,17 @@
 package nightkosh.advanced_fishing.client;
 
+import net.minecraft.client.renderer.entity.FishingHookRenderer;
+import net.minecraft.client.renderer.entity.ItemEntityRenderer;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import nightkosh.advanced_fishing.api.ModInfo;
+import nightkosh.advanced_fishing.core.AFEntities;
 import nightkosh.advanced_fishing.core.AFItems;
 import nightkosh.advanced_fishing.item.ItemBlazingFishingRod;
 
@@ -42,6 +46,13 @@ public class AFClientSetup {
                     return 0;
                 }
         ));
+    }
+
+    @SubscribeEvent
+    public static void entityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(AFEntities.getCustomFishHook(), FishingHookRenderer::new);
+        event.registerEntityRenderer(AFEntities.getLavaFishHook(), FishingHookRenderer::new);
+        event.registerEntityRenderer(AFEntities.getFireproofItem(), ItemEntityRenderer::new);
     }
 
 }
